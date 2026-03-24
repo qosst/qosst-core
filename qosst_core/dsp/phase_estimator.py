38468fd9f6ced31ed1925edca350a13eff1ce254
Thomas Liege <thomas.liege@lip6.fr>
2026-03-24 12:46:09 +0100

    Fix / phase estimator implementation

---
+++ qosst_core/dsp/phase_estimator.py
@@ -0,0 +1,26 @@
+"""
+Generic phase estimator.
+"""
+import abc
+from typing import Optional
+
+import numpy as np
+
+
+# pylint: disable=too-few-public-methods
+class PhaseEstimator(abc.ABC):
+    """
+    Abstract class for the phase estimator.
+    """
+
+    def __init__(self, **_kwargs) -> None:
+        pass
+
+    @abc.abstractmethod
+    def estimate_phase(self) -> np.ndarray:
+        """
+        Estimate the phase.
+
+        Returns:
+            np.ndarray: the estimated phase.
+        """
