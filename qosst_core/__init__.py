b3d0df36597ce646ca9dd6888949c84e24592cd4
Yoann Piétri <Yoann.Pietri@lip6.fr>
2026-09-11 09:04:38 +0200

    feat: Make BaseTimingRecovery more general

--- qosst_core/__init__.py
+++ qosst_core/__init__.py
@@ -26,6 +26,6 @@ It contains :
 * some constants.
 """
 
-__version__ = "0.10.4.dev12"
+__version__ = "0.10.4.dev13"
 
 RELEASE_NAME = "Karadoc"
--- qosst_core/dsp/timing_estimator.py
+++ qosst_core/dsp/timing_estimator.py
@@ -20,26 +20,85 @@ Generic timing recovery estimator.
 """
 
 import abc
+from typing import Optional
 
 import numpy as np
 
 
-# pylint: disable=too-few-public-methods
+# pylint: disable=too-few-public-methods, too-many-instance-attributes
 class BaseTimingRecoveryEstimator(abc.ABC):
     """
     Abstract class for the timing recovery estimator.
     """
 
-    def __init__(self, **_kwargs) -> None:
-        pass
+    sps: float  #: Samples per symbol.
+    adc_rate: float  #: ADC rate in Sample per second.
+    num_symbols: int  #: Total number of symbols.
+    subframe_length: int  #: Number of symbols in a subframe.
+    symbol_timing_oversampling: int  #: Factor by which the signal is oversampled when searching for the optimal symbol sampling time.
+    roll_off: float  #: Roll-off factor of the root raised cosine filter.
+    symbol_rate: float  #: Symbol rate in Baud.
+    pilots_frequencies: np.ndarray[float]  #: Array of frequencies for the pilots.
+    frequency_shift: float  #: Frequency shift of the quantum data.
+    num_samples_previous_subframe: (
+        int  #: Number of samples recovered in the previous subframe.
+    )
+    pulsed_sampling: bool  #: Wether to use the pulse sampling method, which consists in convolving the signal with a rectangular pulse of width equal to the symbol period, and then sampling at the symbol rate.
+
+    # pylint: disable=too-many-arguments, too-many-positional-arguments
+    def __init__(
+        self,
+        sps: float,
+        adc_rate: float,
+        num_symbols: int,
+        subframe_length: int,
+        symbol_timing_oversampling: int,
+        roll_off: float,
+        symbol_rate: float,
+        pilots_frequencies: np.ndarray,
+        frequency_shift: float,
+        num_samples_previous_subframe: int,
+        pulsed_sampling: bool = False,
+    ) -> None:
+        """
+        Args:
+            sps (float): Samples per symbol.
+            adc_rate (float): _desADC rate in Sample per second.cription_
+            num_symbols (int):  Total number of symbols.
+            subframe_length (int): Number of symbols in a subframe.
+            symbol_timing_oversampling (int): actor by which the signal is oversampled when searching for the optimal symbol sampling time.
+            roll_off (float): Roll-off factor of the root raised cosine filter.
+            symbol_rate (float): Symbol rate in Baud.
+            pilots_frequencies (np.ndarray): Array of frequencies for the pilots.
+            frequency_shift (float): Frequency shift of the quantum data.
+            num_samples_previous_subframe (int): Number of samples recovered in the previous subframe.
+            pulsed_sampling (bool, optional): Wether to use the pulse sampling method, which consists in convolving the signal with a rectangular pulse of width equal to the symbol period, and then sampling at the symbol rate. Defaults to False.
+        """
+        self.sps = sps
+        self.adc_rate = adc_rate
+        self.num_symbols = num_symbols
+        self.subframe_length = subframe_length
+        self.symbol_timing_oversampling = symbol_timing_oversampling
+        self.roll_off = roll_off
+        self.symbol_rate = symbol_rate
+        self.pilots_frequencies = pilots_frequencies
+        self.frequency_shift = frequency_shift
+        self.num_samples_previous_subframe = num_samples_previous_subframe
+        self.pulsed_sampling = pulsed_sampling
 
     @abc.abstractmethod
-    def sample(self) -> np.ndarray:
+    def sample(
+        self, data: np.ndarray, pilot_data: Optional[np.ndarray] = None
+    ) -> np.ndarray:
         """
-        Estimate the timing.
+        Estimate the sampling points.
+
+        Args:
+            data (np.ndarray): data to sample
+            pilot_data (Optional[np.ndarray]): Optional data to give that maybe be used by some sampling estimators. Default to None.
 
         Returns:
-            np.ndarray: the estimated timing.
+            np.ndarray: the estimated sampling points.
         """
 
 
@@ -50,7 +109,7 @@ class NoneTimingRecoveryEstimator(BaseTimingRecoveryEstimator):
     Raise a non implemented error.
     """
 
-    def sample(self):
+    def sample(self, data: np.ndarray, pilot_data: Optional[np.ndarray] = None):
         raise NotImplementedError(
             "NoneTimingRecoveryEstimator should not be used in the DSP."
         )
